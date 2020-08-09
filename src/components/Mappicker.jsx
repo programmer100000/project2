a1546bc8351c1b11e66227aa97701a7c0d8a86f6
finter <finter@gmail.com>
2020-08-09 10:18:40 +0430

    map picker done

--- src/components/Mappicker.jsx
+++ src/components/Mappicker.jsx
@@ -1,93 +1,105 @@
-import React, { Fragment, useState } from 'react';
-import MapPicker from 'react-google-map-picker';
+import React, { Fragment } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-
-
-
-const Mappicker = () => {
-
-    const [defaultLocationLat, setDefaultLocationLat] = useState(10);
-    const [defaultLocationLng, setDefaultLocationLng] = useState(106);
-    const DefaultLocation = { lat: defaultLocationLat, lng: defaultLocationLng };
-    const [defaultLoc, setdefaultLoc] = useState(DefaultLocation);
-    const [location, setLocation] = useState(DefaultLocation);
-
-    function handleChangeLocation(event,lat, lng) {
-        event.preventDefault();
-        setLocation({ lat: lat, lng: lng });
-     alert(location.lat);
-    alert(location.lng);
+export class Mappicker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stores: [{ lat: 47.49855629475769, lng: -122.14184416996333 }],
+            store: '',
+        };
     }
+    addLocation = (event) => {
+        event.preventDefault();
+        const stores = [...this.state.stores];
+        const store = {
+            lat: this.state.lat,
+            lng: this.state.lng,
+        };
+        stores.push(store);
+        console.log(stores);
+        this.setState({ stores });
+    };
+    displayMarkers = () => {
+        return this.state.stores.map((store, index) => {
+            return (
+                <Marker
+                    key={index}
+                    id={index}
+                    position={{
+                        lat: store.lat,
+                        lng: store.lng,
+                    }}
+                    onClick={() => console.log('You clicked me!')}
+                />
+            );
+        });
+    };
 
-
-
-    // function handleResetLocation() {
-    //     setdefaultLoc({ ...DefaultLocation });
-
-    // }
-
-
-
-    // const [locationLat, setlocationLat] = useState(defaultLocationLat);
-    // const [locationLng, setlocationLng] = useState(defaultLocationLng);
-    // const location = { lat: defaultLocationLat, lng: defaultLocationLng };
-    // function handleChangeLocation() {
-    //     setlocationLat(locationLat);
-    //     setlocationLng(locationLng);
-    // }
-    // function handleResetLocation() {
-    //     setDefaultLocationLat(defaultLocationLat);
-    //     setDefaultLocationLng(defaultLocationLng);
-
-    // }
-
-    return (
-        <Fragment>
-            <div className="container">
-                <div className="row p-4">
-                    <div className="col-md-4 m-auto">
-                        <form  >
-                            <div className="form-group">
-                                <textarea className="form-control" name="lat" id="lat" required
-                                    type='text' value={location.lat} placeholder='latitute'
+    render() {
+        return (
+            <Fragment>
+                <div className='row w-100 p-4' style={{ height: '500px' }}>
+                    <div className='col-md-4 '>
+                        <form>
+                            <div className='form-group'>
+                                <textarea
+                                    className='form-control'
+                                    name='lat'
+                                    id='lat'
+                                    required
+                                    type='text'
+                                    value={this.state.lat}
+                                    placeholder='latitute'
                                     onChange={(e) => {
-                                        setLocation({...location, lat:e.target.value });
+                                        this.setState({
+                                            lat: e.target.value,
+                                        });
                                     }}
-                                >
-                                </textarea>
+                                ></textarea>
                             </div>
-                            <div className="form-group">
-                                <textarea className="form-control" name="lng" id="lng" required
-                                    type='text' value={location.lng} placeholder='longitute'
+                            <div className='form-group'>
+                                <textarea
+                                    className='form-control'
+                                    name='lng'
+                                    id='lng'
+                                    required
+                                    type='text'
+                                    value={this.state.lng}
+                                    placeholder='longitute'
                                     onChange={(e) => {
-                                        setLocation({...location, lng:e.target.value });
+                                        this.setState({
+                                            lng: e.target.value,
+                                        });
                                     }}
-                                >
-                                </textarea>
+                                ></textarea>
                             </div>
-                            {/* <div className="form-group">
-                                <input type='text' value={zoom} disabled />
-                                </div> */}
-                            <div className="form-group mb-0 text-center">
-                                <button className="btn btn-success btn-block" onClick={handleChangeLocation} > ریست   </button>
+                            <div className='form-group mb-0 text-center'>
+                                <button
+                                    className='btn btn-success btn-block'
+                                    onClick={this.addLocation}
+                                >
+                                    {' '}
+                                    ثبت{' '}
+                                </button>
                             </div>
                         </form>
                     </div>
-                    <div className="col-md-8 ">
-                        <MapPicker defaultLocation={location}
-                            // zoom={zoom}
-                            style={{ height: '700px' }}
-                            onChangeLocation={handleChangeLocation}
-                            // onChangeZoom={handleChangeZoom}
-                            apiKey='AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8' />
+                    <div className='col-md-8'>
+                        <Map
+                            google={this.props.google}
+                            zoom={8}
+                            initialCenter={{ lat: 47.444, lng: -122.176 }}
+                        >
+                            {this.displayMarkers()}
+                        </Map>
                     </div>
                 </div>
-            </div>
-        </Fragment>
-
-
-
-    );
+            </Fragment>
+        );
+    }
 }
 
-export default Mappicker;
+export default GoogleApiWrapper({
+    // apiKey: 'TOKEN HERE'
+})(Mappicker);
